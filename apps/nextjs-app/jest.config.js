4b7e10ff0970aae297668ceeb6a43dac8e30d0f0
BoBeenLee <globaldev@naver.com>
2022-03-17 16:11:28 +0900

    refactor: move to apps/apps -> apps

--- apps/nextjs-app/jest.config.js
+++ apps/nextjs-app/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  displayName: 'apps-nextjs-app',
-  preset: '../../../jest.preset.js',
+  displayName: 'nextjs-app',
+  preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../../coverage/apps/apps/nextjs-app',
+  coverageDirectory: '../../coverage/apps/nextjs-app',
 };
