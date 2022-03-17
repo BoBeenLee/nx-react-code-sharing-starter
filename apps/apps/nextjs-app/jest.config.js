2b931ae40babb7e8317919df9c5cdcd15be1f1b9
BoBeenLee <globaldev@naver.com>
2022-03-17 13:59:24 +0900

    refactor: rename nextjs-apps

--- apps/apps/nextjs-app/jest.config.js
+++ apps/apps/nextjs-app/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  displayName: 'nx-react-code-sharing',
-  preset: '../../jest.preset.js',
+  displayName: 'apps-nextjs-app',
+  preset: '../../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/nx-react-code-sharing',
+  coverageDirectory: '../../../coverage/apps/apps/nextjs-app',
 };
