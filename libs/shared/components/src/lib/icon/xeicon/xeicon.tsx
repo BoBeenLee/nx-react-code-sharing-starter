2869f3adc3653e4275193bf53579d6c3c25c4010
BoBeenLee <globaldev@naver.com>
2022-04-10 21:47:55 +0900

    add test xeicon testtype

--- libs/shared/components/src/lib/icon/xeicon/xeicon.tsx
+++ libs/shared/components/src/lib/icon/xeicon/xeicon.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import styled from '@emotion/primitives';
 
+
 /* eslint-disable-next-line */
 export interface XEIconProps {}
 
 const StyledXEIcon = styled.Text`
   color: pink;
 `;
+
+type TestType = NoUndefinedField<{ a?: string }>;
 
 export function XEIcon(props: XEIconProps) {
   return <StyledXEIcon>XEIcon</StyledXEIcon>;
