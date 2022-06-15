30d0c9de4e8e6c907ed7d07182dd65aff064c684
bobinlee <bobeen.lee@huiseoul.com>
2022-06-15 14:33:53 +0900

    feat: add mock asserts function method

--- libs/sdk/src/lib/sdk.ts
+++ libs/sdk/src/lib/sdk.ts
@@ -1,3 +1,16 @@
-export function sdk(): string {
-  return 'sdk';
+export class SDK {
+  constructor(public loggedInUserId?: string) {}
+
+  createTodo() {
+    this.asswertUserIsLoggedIn();
+    console.log("loggedInUserId is string", this.loggedInUserId);
+  }
+
+  asswertUserIsLoggedIn(): asserts this is this & {
+    loggedInUserId: string;
+  } {
+    if(!this.loggedInUserId) {
+      throw new Error("User is not logged in");
+    }
+  }
 }
