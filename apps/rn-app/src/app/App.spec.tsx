56d0c8e2391f4ffb593d519ede98a30070b0889b
bobeenlee <globaldev@naver.com>
2022-07-11 12:04:43 +0900

    chore: remove rn-app

--- apps/rn-app/src/app/App.spec.tsx
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import { render } from '@testing-library/react-native';
-
-import App from './App';
-
-test('renders correctly', () => {
-  expect('1').toBe('1');
-});
