68e4b0b531f86134d3e7952dbf03d10543eb059a
bobeenlee <globaldev@naver.com>
2022-07-20 19:09:33 +0900

    fix: fixed codefator issue

--- apps/nextjs-app/pages/api/healthz.tsx
+++ apps/nextjs-app/pages/api/healthz.tsx
@@ -1,7 +1,9 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+export const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'application/json');
   res.end(JSON.stringify({ status: 'ok' }));
 };
+
+export default handler;
