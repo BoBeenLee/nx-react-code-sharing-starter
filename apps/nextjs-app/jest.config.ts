6ce5944fe87678ee2ed84183c469108a65b8539d
bobeenlee <globaldev@naver.com>
2022-07-20 15:36:40 +0900

    fix: fixed nextjs-app error testcode

--- apps/nextjs-app/jest.config.ts
+++ apps/nextjs-app/jest.config.ts
@@ -4,7 +4,13 @@ export default {
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
-    '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
+    '^.+\\.[tj]sx?$': [
+      'babel-jest',
+      { presets: ['@nrwl/next/babel'], babelrc: false },
+    ],
+  },
+  moduleNameMapper: {
+    '^react-native$': 'react-native-web',
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
   coverageDirectory: '../../coverage/apps/nextjs-app',
