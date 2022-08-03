cba406d5ce9be02a938c32759c9b289298b9fc6d
bobeenlee <globaldev@naver.com>
2022-08-03 10:26:09 +0900

    feat: remove atomic design comp structure

--- libs/shared/ui-components/src/lib/text/text.tsx
+++ libs/shared/ui-components/src/lib/text/text.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { StyleSheet, Text, TextProps as RNTextProps } from 'react-native';
-import { colors } from "../../styles/colors";
+import { colors } from "../styles/colors";
 
 type FontWeight = 'BOLD' | 'MEDIUM' | 'REGULAR';
 
