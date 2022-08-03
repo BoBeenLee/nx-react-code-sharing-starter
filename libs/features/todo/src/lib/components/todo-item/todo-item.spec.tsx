d2d8d8a7be7f7cabf6fc4a5cca4f53decf1f3a0f
bobeenlee <globaldev@naver.com>
2022-08-03 10:10:18 +0900

    fix: add children props

--- libs/features/todo/src/lib/components/todo-item/todo-item.spec.tsx
+++ libs/features/todo/src/lib/components/todo-item/todo-item.spec.tsx
@@ -4,9 +4,7 @@ import TodoItem from './todo-item';
 
 describe('TodoItem', () => {
   it('should render successfully', () => {
-    const { baseElement } = render(
-      <TodoItem />
-    );
+    const { baseElement } = render(<TodoItem>Helloworld</TodoItem>);
     expect(baseElement).toBeTruthy();
   });
 });
--- libs/features/todo/src/lib/components/todo-item/todo-item.tsx
+++ libs/features/todo/src/lib/components/todo-item/todo-item.tsx
@@ -6,10 +6,13 @@ const styles = StyleSheet.create({
   },
 });
 
-export interface TodoItemProps {}
+export interface TodoItemProps {
+  children: React.ReactNode;
+}
 
 export function TodoItem(props: TodoItemProps) {
-  return <Text style={styles.TodoItem}>hello world</Text>;
+  const { children } = props;
+  return <Text style={styles.TodoItem}>{children}</Text>;
 }
 
 export default TodoItem;
