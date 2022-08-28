44d05c4c96a061f7c624b862639b654d66392c03
bobeenlee <globaldev@naver.com>
2022-08-28 16:59:42 +0900

    feat: add image comp

--- libs/shared/ui-components/src/lib/image/image.tsx
+++ libs/shared/ui-components/src/lib/image/image.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import { Image as RNImage, ImageProps as RNImageProps } from 'react-native';
 
 /* eslint-disable-next-line */
-export interface ImageProps extends RNImageProps {}
+export interface ImageProps extends RNImageProps {
+  alt: string;
+}
 
 export function Image(props: ImageProps) {
   return <RNImage {...props} />;
